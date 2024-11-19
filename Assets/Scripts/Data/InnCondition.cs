3cec61fda43ea201e2b0c857b5827c4f0a8776db
RaderRMT <raderpy@protonmail.com>
2024-11-19 14:18:10 +0100

    added custom editor for inn conditions

--- Assets/Scripts/Data/InnCondition.cs
+++ Assets/Scripts/Data/InnCondition.cs
@@ -2,9 +2,48 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
+    public enum ObjectType
+    {
+        Beer,
+        Food,
+        Noise,
+        Number,
+    }
+
+    public enum Comparison
+    {
+        Less,
+        LessOrEqual,
+        Greater,
+        GreaterOrEqual,
+        Equals,
+        AboveOrBelow
+    }
+
+    public enum Place
+    {
+        Inn,
+        Bar,
+    }
+    
     [CreateAssetMenu(fileName = "InnCondition", menuName = "Data/Condition/InnCondition", order = 0)]
     public class InnCondition : ScriptableObject
     {
-        
+        public ObjectType Left;
+        public Comparison Comparison;
+        public ObjectType Right;
+        public Place Place;
+        public int Number;
+
+        public bool CheckCondition()
+        {
+            return false;
+        }
     }
 }
+
+/*
+OBJECT COMPARISON COUNT PLACE
+OBJECT ABOVEORBELOW
+OBJECT COMPARISON OBJECT
+ */
---
+++ Assets/Scripts/Editor/InnConditionEditor.cs
@@ -0,0 +1,53 @@
+﻿using DefaultNamespace;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using Comparison = DefaultNamespace.Comparison;
+
+[CustomEditor(typeof(InnCondition))]
+public class InnConditionEditor : Editor
+{
+    private SerializedProperty _left;
+    private SerializedProperty _comparison;
+    private SerializedProperty _right;
+    private SerializedProperty _number;
+    private SerializedProperty _place;
+    
+    private void OnEnable()
+    {
+        _left = serializedObject.FindProperty("Left");
+        _comparison = serializedObject.FindProperty("Comparison");
+        _right = serializedObject.FindProperty("Right");
+        _number = serializedObject.FindProperty("Number");
+        _place = serializedObject.FindProperty("Place");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PropertyField(_left, new GUIContent(""));
+        EditorGUILayout.PropertyField(_comparison, new GUIContent(""));
+        Comparison comparisonType = (Comparison) _comparison.GetUnderlyingValue();
+        if (comparisonType == DefaultNamespace.Comparison.AboveOrBelow)
+        {
+            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.EndHorizontal();
+            return;
+        }
+        
+        EditorGUILayout.PropertyField(_right, new GUIContent(""));
+        EditorGUILayout.EndHorizontal();
+
+        ObjectType type = (ObjectType) _right.GetUnderlyingValue();
+        if (type == ObjectType.Number)
+        {
+            ObjectType leftType = (ObjectType) _left.GetUnderlyingValue();
+            EditorGUILayout.PropertyField(_number, new GUIContent($"{leftType.HumanName()} Count"));
+        }
+        
+        EditorGUILayout.PropertyField(_place, new GUIContent("In"));
+        serializedObject.ApplyModifiedProperties();
+    }
+}

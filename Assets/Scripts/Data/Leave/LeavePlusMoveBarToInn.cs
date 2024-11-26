9cfb4c613ba079fed65330352d4da8f8aa3e7e32
RaderRMT <raderpy@protonmail.com>
2024-11-26 10:57:59 +0100

    added more conditions

---
+++ Assets/Scripts/Data/Leave/LeavePlusMoveBarToInn.cs
@@ -0,0 +1,18 @@
+﻿using Card;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Leave Plus Move Bar To Inn", menuName = "Data/Leave/Leave Plus Move Bar To Inn", order = 0)]
+public class LeavePlusMoveBarToInn : AbstractLeave
+{
+    public override int Execute(CardData card)
+    {
+        int index = GameManager.Instance.inn.GetCardPosition(card);
+        
+        CardData tmp = GameManager.Instance.inn.CardsInInn[index];
+        GameManager.Instance.inn.CardsInInn[index] = GameManager.Instance.inn.CardsInInn[0];
+        GameManager.Instance.inn.CardsInInn[0] = tmp;
+        
+        GameManager.Instance.inn.RemoveCardAt(0);
+        return -1;
+    }
+}
---
+++ Assets/Scripts/Data/Leave/LeavePlusTopCardInDeck.cs
@@ -0,0 +1,15 @@
+﻿using Card;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Leave Plus Top Card In Deck", menuName = "Data/Leave/Leave Plus Top Card In Deck", order = 0)]
+public class LeavePlusTopCardInDeck : AbstractLeave
+{
+    public override int Execute(CardData card)
+    {
+        GameManager.Instance.inn.RemoveCard(card);
+
+        GameManager.Instance.deck.DrawCard();
+        
+        return 0;
+    }
+}
